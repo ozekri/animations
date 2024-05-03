88c782a3158812e80f89771f41fa2ccf902ce0a4
Oussama Zekri <149584614+ozekri@users.noreply.github.com>
2024-05-03 14:29:30 +0200

    Add files via upload

--- kernel_trick/ktrick.py
+++ kernel_trick/ktrick.py
@@ -31,21 +31,45 @@ scatter = ax.scatter(X[:, 0], X[:, 1], np.zeros_like(X[:, 0]), c=y, cmap=plt.cm.
 ax.set_xlim([-2, 2])
 ax.set_ylim([-2, 2])
 ax.set_zlim([-2, 2])  # Pour inclure la variation sur l'axe z
-ax.view_init(azim=-60, elev=20)
+ax.view_init(azim=-60, elev=20) #90;-90
 ax.set_xticks([])
 ax.set_yticks([])
 ax.set_zticks([])
-
+plt.show()
+#ax.axis('off')
+# Tracer le plan z=0 en gris
+x0, y0 = np.meshgrid(np.linspace(-2, 2, 2), np.linspace(-2, 2, 2))
+z0 = np.zeros_like(x0)
+ax.plane_surface = ax.plot_surface(x0, y0, z0, color='gray', alpha=0.3)
+ax.hyperplan = None
 # Calculer les positions interpolées des points de données
 def calculate_interpolated_positions(X, X_3d, frame):
     interpolated_positions = X + (X_3d - X) * (frame / 100)
     return interpolated_positions
 
+# Calculer l'opacité en fonction de la frame
+def calculate_opacity(frame):
+    if frame <= 0:
+        return -frame * 0.03  # Réduire l'opacité de 0.5 à 0.0 sur les 10 premières frames
+    else:
+        return 0.0
+
 # Fonction pour mettre à jour l'animation
 def update(frame):
-    interpolated_positions = calculate_interpolated_positions(X, X_3d, frame)  # Nous utilisons X_3d pour les positions finales
-    # Mettre à jour les positions des points de données
-    scatter._offsets3d = (interpolated_positions[:, 0], interpolated_positions[:, 1], interpolated_positions[:, 2])
+    ax.view_init(azim=-frame, elev=20)
+    opacity = calculate_opacity(frame)
+    ax.plane_surface.set_visible(False)
+        
+    if frame <= 0:
+        ax.plane_surface = ax.plot_surface(x0, y0, z0, color='gray', alpha=opacity)
+        if ax.hyperplan is not None:
+            ax.hyperplan.set_visible(False)
+
+    if frame > 0:
+        ax.axis('on')
+        interpolated_positions = calculate_interpolated_positions(X, X_3d, frame)  # Nous utilisons X_3d pour les positions finales
+        # Mettre à jour les positions des points de données
+        scatter._offsets3d = (interpolated_positions[:, 0], interpolated_positions[:, 1], interpolated_positions[:, 2])
     
     # Ajouter un plan séparateur dans les dernières frames de l'animation
     if frame >= 85:
@@ -63,18 +87,20 @@ def update(frame):
         xx, yy = np.meshgrid(np.linspace(-1.5, 1.5, 10), np.linspace(-1.5, 1.5, 10))
         zz = (-normal_vector[0] * xx - normal_vector[1] * yy) / normal_vector[2]
         # Tracer le plan séparateur
-        ax.plot_surface(xx, yy, zz, color='gray', alpha=0.5,label="Hyperplane")
+        ax.hyperplan = ax.plot_surface(xx, yy, zz, color='orange', alpha=0.5,label="Hyperplane")
     
     return scatter,
 
 # Créer l'animation
-ani = FuncAnimation(fig, update, frames=np.linspace(0, 100, 100), interval=30)
-
-for i, frame in enumerate(np.linspace(0, 100, 100)):
+ani = FuncAnimation(fig, update, frames=np.linspace(-10, 100, 100), interval=30)
+plt.show()
+"""
+for i, frame in enumerate(np.linspace(-10, 100, 100)):
     update(frame)
     if i<=9:
         plt.savefig(f"gif_saves/frame_0{i}.png",dpi=300, format='png', bbox_inches='tight')
     else:
         plt.savefig(f"gif_saves/frame_{i}.png",dpi=300, format='png', bbox_inches='tight')
-
 plt.show()
+
+"""
